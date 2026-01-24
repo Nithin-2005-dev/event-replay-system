59da7c140af036359d58360276ea40ae816297ff
Nithin-2005-dev <nithinkumar2005b@gmail.com>
2026-01-24 14:44:10 +0530

    Clean Spring Boot setup without database

---
+++ src/test/java/com/nithin/eventreplay/EventReplaySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nithin.eventreplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventReplaySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
